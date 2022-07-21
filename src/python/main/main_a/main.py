fc9377f095ed06fbd4f193d704cb8e6091e2f999
ebiebix <ebi.h.821@gmail.com>
2022-07-21 20:13:36 +0900

    aaa

--- src/python/main/main_a/main.py
+++ src/python/main/main_a/main.py
@@ -1,7 +1,6 @@
 from lib.lib_a.sample import sample
-from lib.lib_a.log import set_logging_basic_config
+from lib/lib_a/array.py
 
 if __name__ == "__main__":
-    set_logging_basic_config("INFO")
-    print("hello world!")
+
     sample()

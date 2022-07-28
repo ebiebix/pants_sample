e2316f2b60c8fe859db1a8b1de1d0c6cf70b392f
ebiebix <ebi.h.821@gmail.com>
2022-07-28 17:25:21 +0900

    test

--- src/python/lib/lib_a/sample.py
+++ src/python/lib/lib_a/sample.py
@@ -1,5 +1,2 @@
-from lib.lib_a.log import set_logging_basic_config
-
-
 def sample():
     print("hello from sample")
--- src/python/main/main_a/main.py
+++ src/python/main/main_a/main.py
@@ -1,6 +1,10 @@
 from lib.lib_a.sample import sample
-from lib/lib_a/array.py
+from lib.lib_a.array import get_zero_array, get_zero_tf_array
 
 if __name__ == "__main__":
-
+    shape = [3, 3, 3]
+    zero_np = get_zero_array(shape)
+    zero_tf = get_zero_tf_array(shape)
     sample()
+    print(zero_np)
+    print(zero_tf)
